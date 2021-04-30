a964fd9921ca2ad43728dd01a43345159efe3416
aedigo <aedigo12@hotmail.com>
2021-04-30 16:21:47 -0300

    New Changes!

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -143,6 +143,7 @@ floating_layout = layout.Floating(float_rules=[
     Match(wm_class='maketag'),  # gitk
     Match(wm_class='ssh-askpass'),  # ssh-askpass
     Match(wm_class='steam'),  # ssh-askpass
+    Match(wm_class='guake'),  # ssh-askpass
     Match(title='branchdialog'),  # gitk
     Match(title='pinentry'),  # GPG key password entry
 ])
