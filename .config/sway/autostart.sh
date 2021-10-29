f33f36b91ce0590e552d8c3607d4f78f632db443
aedigo <aedigo12@hotmail.com>
2021-10-29 18:29:10 -0300

    New Changes!

---
+++ .config/sway/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This will define the backgroundcolor
+swaybg -c '#282828' -m fill &
+safeeyes &
+# This will enable night mode
+gammastep -O 2000
+# This will autostart searx
+searxStart &
+whatsapp-nativefier &
