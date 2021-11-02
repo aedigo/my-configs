1337e8afa6724851d80fb6dd6bf314423bd32d88
aedigo <aedigo12@hotmail.com>
2021-11-02 08:38:11 -0300

    New Changes!

--- .config/sway/autostart.sh
+++ .config/sway/autostart.sh
@@ -3,12 +3,7 @@
 swaybg -c '#282828' -m fill &
 safeeyes &
 # This will set transparecy on inactive windows
-inactive_trans -o .4 &
 # This will enable night mode
 wlsunset -t 3000 -T 8000 -g .8 &
-# This will autostart searx
-searxStart &
 dunst &
 whatsapp-nativefier &
-main-layout &
-
