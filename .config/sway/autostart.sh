38294c87d85dd6b206ff4c251276334bd9978feb
aedigo <aedigo12@hotmail.com>
2021-11-02 08:30:42 -0300

    New Changes!

--- .config/sway/autostart.sh
+++ .config/sway/autostart.sh
@@ -2,8 +2,10 @@
 # This will define the backgroundcolor
 swaybg -c '#282828' -m fill &
 safeeyes &
+# This will set transparecy on inactive windows
+inactive_trans -o .4 &
 # This will enable night mode
-gammastep -O 2000
+wlsunset -t 3000 -T 8000 -g .8 &
 # This will autostart searx
 searxStart &
 dunst &
