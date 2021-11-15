0f525343d3111351a4d8b0caa1c30d95b6c445f8
aedigo <aedigo12@hotmail.com>
2021-11-15 17:35:44 -0300

    New Changes!

--- .config/sway/autostart.sh
+++ .config/sway/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This will define the backgroundcolor
-swaybg -c '#282828' -m fill &
+swaybg -c '#282a36' -m fill &
 safeeyes &
 # This will set transparecy on inactive windows
 # This will enable night mode
-wlsunset -t 3000 -T 8000 -g .8 &
+wlsunset -t 3000 &
 dunst &
 whatsapp-nativefier &
