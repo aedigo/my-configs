95bd6ec97f71bf14d2b9cb4e6f36ab355d4d37f6
aedigo <aedigo12@gmail.com>
2021-11-24 16:25:42 -0300

    New Changes!

--- .config/qutebrowser/searchengines.py
+++
@@ -1,2 +0,0 @@
-searchengines = {'DEFAULT': 'localhost/search?hl=en&q={}',  'go': 'https://www.google.com/search?hl=en&q={}', "wa": "https://wiki.archlinux.org/?search={}", 'wk': "https://www.wikipedia.org/w/index.php?title=Special:Search&search={}", 'mdn': "https://developer.mozilla.org/en-US/search?q={}", 'swk': 'https://simple.wikipedia.org/wiki/{}'}
-
