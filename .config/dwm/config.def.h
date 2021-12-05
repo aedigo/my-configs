ca87eeb11d4a9f32517e420edf09e4823e6dbf9e
aedigo <aedigo12@gmail.com>
2021-12-05 19:33:20 -0300

    New Changes!

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -42,7 +42,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx*/
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1,        50,50,500,500,        5 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,        50,50,500,500,        5 },
-	{ NULL,       NULL,   "scratchpad",   0,            1,           -1,       's', 50,50,500,500,    5},
+	{ NULL,       NULL,   "scratchpad",   0,            1,           -1,       's', 750,10,600,300,    5},
 	/* class      instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx*/
 		/*{ NULL,		    "spfm",		NULL,		    SPTAG(0),		  1,			     -1,        50,50,500,500,        5}, */
 };
