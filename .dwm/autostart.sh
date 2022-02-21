e40170d1e7251062ef044f30880ce30759bad5a8
aedigo <aedigo12@gmail.com>
2022-02-21 06:47:43 -0300

    New Changes!

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-hsetroot -solid '#282a36' &
+hsetroot -solid '#282828' &
 xrandr --output VGA1 --gamma 1.0:0.88:0.76 --brightness 0.85 &
 alsactl --file ~/.config/asound.state restore &
 xmodmap ~/.Xmodmap &
 unclutter &
+jamesdsp -t &
+run_xidlehook &
