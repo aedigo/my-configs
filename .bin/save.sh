405ccb6120f8649c66560399a13e9c9e9dd3e7f2
aedigo <aedigo12@hotmail.com>
2022-05-30 13:48:41 -0300

    new changes!

--- .bin/save.sh
+++ .bin/save.sh
@@ -9,12 +9,18 @@ copy() {
   cp $1 $2 -a
 }
 
+delete() {
+  rm -rf "$@" 
+}
+
 save() {
   cd $path
   if [[ $3 = 'cg' ]] 
   then
+    delete $path/$2/$1
     copy $home/.config/$1 $path/$2
   else 
+    delete $path/$1
     copy $home/$1 $path/$2
   fi
   git add . ;
