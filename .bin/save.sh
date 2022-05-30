f41171c79987a6e604699949ed83fb56b5dd6a4e
aedigo <aedigo12@hotmail.com>
2022-05-30 13:55:06 -0300

    new changes!

--- .bin/save.sh
+++ .bin/save.sh
@@ -4,6 +4,15 @@ dir=$(pwd)
 path=~/.my-configs
 home=/home/aedigo
 
+if [[ $1 = '--help' ]]; then
+  echo "
+  To add a config to the main my-configs dir use: filename(inside your home folder), thats it. Ex: save.sh .dwm
+
+  To add a config that is inside the .config dir and to put it inside the .config dir on my-configs folder use: filename(inside your .config folder) path(inside your .config) cg. Ex: save.sh sway .config cg
+  "
+  exit 1
+fi
+
 copy() {
   echo $1 $2
   cp $1 $2 -a
