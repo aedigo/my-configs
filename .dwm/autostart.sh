cbad6416269e99fd228d249be05557c8548cf17d
aedigo <aedigo12@hotmail.com>
2022-05-31 12:51:25 -0300

    new changes!

---
+++ .dwm/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+hsetroot -solid '#282a36' &
+xrandr --output VGA-1 --gamma 1.0:0.88:0.76 --brightness 0.85 &
+easyeffects --gapplication-service &
+dunst &
+xmodmap ~/.Xmodmap &
+unclutter &
+run_xidlehook &
+safeeyes &
+xcape -e 'Control_L=Escape' &
+st &
